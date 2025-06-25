a79364c91f2895b8e9a0857a1beeccfc5e2fa133
Sagar Patil <sagarp@perscitussln.com>
2025-06-25 16:07:52 +0530

    test

--- server/index.ts
+++ server/index.ts
@@ -56,10 +56,10 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  const port = process.env.PORT || 5000;
+  const port = process.env.PORT || 3000;
   server.listen({
     port: +port,
-    host: "0.0.0.0",
+    // host: "0.0.0.0",
     reusePort: true,
   }, () => {
     log(`Server running on http://localhost:${port}`);
